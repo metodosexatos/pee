2065d73b55b3aa6fbb599acc6b86fda4c2bc9db2
Métodos Exatos <37481514+metodosexatos@users.noreply.github.com>
2019-04-21 19:21:18 -0300

    Add files via upload

---
+++ numeros_aleatorios.R
@@ -0,0 +1,21 @@
+#############################
+####### Métodos Exatos ######
+### www.metodosexatos.com ###
+#############################
+
+# Autor: André Santos | andre@metodosexatos.com.br
+# 18/04/2019
+
+# citation()
+
+# To cite R in publications use:
+
+# R Core Team (2018). R: A language and environment for
+# statistical computing. R Foundation for Statistical
+# Computing, Vienna, Austria. URL https://www.R-project.org/.
+
+####################################################################
+
+# Gerar números aleatórios no R
+
+sample(1:10, 4, replace = FALSE)
